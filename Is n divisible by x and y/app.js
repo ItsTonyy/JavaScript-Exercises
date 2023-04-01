7659c5d05c5e7778b985d8d6809ed4031304b6d8
Tony <tonytnportela@outlook.com>
2023-04-01 18:54:08 -0300

    removi um exercício no commit anterior sem querer

---
+++ Is n divisible by x and y/app.js
@@ -0,0 +1,14 @@
+function isDivisible(n, x, y) {
+  const firstDivision = n % x;
+  const secondDivision = n % y;
+  
+  
+  if (firstDivision > 0 || secondDivision > 0 ) {
+    return false
+  } else {
+    return true
+  }
+}
+
+console.log(isDivisible(8, 4, 2));
+//true
